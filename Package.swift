5d0ba96c6de1bc5a0576bc220abf86cfa83eb3d5
Yurii Boiko <Yurssoft@users.noreply.github.com>
2024-09-07 14:28:12 -0700

    A

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "FusionDI",
+    products: [
+        .library(
+            name: "FusionDI",
+            targets: ["FusionDI"]),
+    ],
+    targets: [
+        .target(
+            name: "FusionDI"),
+    ]
+)
