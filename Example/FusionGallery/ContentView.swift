af36874805dcb7151c10b3656cebd8bbba2031e0
Yurii Boiko <Yurssoft@users.noreply.github.com>
2024-10-10 10:54:50 -0700

    Add proj

---
+++ Example/FusionGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FusionGallery
+//
+//  Created by Yurii Boiko on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/FusionGallery/FusionGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FusionGalleryApp.swift
+//  FusionGallery
+//
+//  Created by Yurii Boiko on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FusionGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
