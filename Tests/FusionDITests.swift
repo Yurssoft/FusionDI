35e46405c76428204583b8f2f60571cb4ef56f79
Yurii Boiko <Yurssoft@users.noreply.github.com>
2024-11-07 08:45:18 -0800

    Update FusionDITests.swift

--- Tests/FusionDITests.swift
+++ Tests/FusionDITests.swift
@@ -10,9 +10,26 @@ import FusionDI
 
 struct Test {
     final class Dependency { }
+    
     @Test func testRegisteringDependency() async throws {
-        ServiceResolver.shared.register(Dependency.self) { Dependency() }
+        let type = Dependency.self
+        ServiceResolver.shared.register(type) { Dependency() }
         #expect(ServiceResolver.shared.registeredDependenciesCreationClosured.count == 1)
+        
+        let key = String(describing: type)
+        #expect(ServiceResolver.shared.registeredDependenciesCreationClosured[key] != nil)
     }
-
+    
+    @Test func testRegisteringResolving() async throws {
+        let type = Dependency.self
+        let object = Dependency()
+        ServiceResolver.shared.register(type) { object }
+        #expect(ServiceResolver.shared.registeredDependenciesCreationClosured.count == 1)
+        
+        let key = String(describing: type)
+        #expect(ServiceResolver.shared.registeredDependenciesCreationClosured[key] != nil)
+        
+        let dependency = ServiceResolver.shared.forceResolve(type)
+        #expect(dependency === object)
+    }
 }
