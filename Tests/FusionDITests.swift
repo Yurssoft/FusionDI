80514c4c122cc673945004c34ee71355f0ac9a8b
Yurii Boiko <Yurssoft@users.noreply.github.com>
2024-11-07 09:01:50 -0800

    Update FusionDITests.swift

--- Tests/FusionDITests.swift
+++ Tests/FusionDITests.swift
@@ -12,6 +12,7 @@ struct Test {
     final class Dependency { }
     
     @Test func testRegisteringDependency() async throws {
+        ServiceResolver.shared.clearAllServiceCaches()
         let type = Dependency.self
         ServiceResolver.shared.register(type) { Dependency() }
         #expect(ServiceResolver.shared.registeredDependenciesCreationClosured.count == 1)
@@ -21,6 +22,7 @@ struct Test {
     }
     
     @Test func testRegisteringResolving() async throws {
+        ServiceResolver.shared.clearAllServiceCaches()
         let type = Dependency.self
         let object = Dependency()
         ServiceResolver.shared.register(type) { object }
@@ -34,6 +36,7 @@ struct Test {
     }
     
     @Test func testRegisteringResolvingWithClearCache() async throws {
+        ServiceResolver.shared.clearAllServiceCaches()
         let type = Dependency.self
         ServiceResolver.shared.register(type) { Dependency() }
         #expect(ServiceResolver.shared.registeredDependenciesCreationClosured.count == 1)
@@ -50,5 +53,38 @@ struct Test {
         let key2 = ObjectIdentifier(dependency2)
         
         #expect(key1 != key2)
+    }
+    
+    @Test func testClearCache() async throws {
+        ServiceResolver.shared.clearAllServiceCaches()
+        let type = Dependency.self
+        ServiceResolver.shared.register(type) { Dependency() }
+        #expect(ServiceResolver.shared.registeredDependenciesCreationClosured.count == 1)
+        
+        let key = String(describing: type)
+        #expect(ServiceResolver.shared.registeredDependenciesCreationClosured[key] != nil)
+        
+        let dependency1 = ServiceResolver.shared.forceResolve(type)
+        let key1 = ObjectIdentifier(dependency1)
+        
+        ServiceResolver.shared.clearServiceCache()
+        
+        let dependency2 = ServiceResolver.shared.forceResolve(type)
+        let key2 = ObjectIdentifier(dependency2)
+        
+        #expect(key1 != key2)
+        ServiceResolver.shared.clearAllServiceCaches()
+        
+        do {
+            let dependency3 = try ServiceResolver.shared.resolve(type)
+            fatalError(String(describing: dependency3))
+        } catch let error {
+            if let serviceError = error as? ServiceResolver.ServiceError {
+                #expect(serviceError == ServiceResolver.ServiceError.absentCreationClosure)
+            } else {
+                fatalError()
+            }
+        }
+        
     }
 }
