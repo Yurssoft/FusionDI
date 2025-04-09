d31c0d799bc5cbb703177f0739bd5399cb043a1e
Yurii Boiko <Yurssoft@users.noreply.github.com>
2025-04-09 09:05:20 -0700

    Update DependenciesSetup.swift

--- Example/FusionGallery/Dependecies/DependenciesSetup.swift
+++ Example/FusionGallery/Dependecies/DependenciesSetup.swift
@@ -8,9 +8,12 @@
 import Foundation
 import FusionDI
 import PhotosServiceProtocols
-import GalleryList
+import DependencyDefinitions
 
 extension PhotosServiceDependency {
+    /*
+     This module is the single point where the production implementation is instantiated or consumed. By isolating the production code here, other modules remain lightweight and avoid compiling the heavy service implementation. This design ensures that the majority of the application can rely on lightweight mocks, while only the designated module handles the heavier production dependency.
+     */
     public static var prod: PhotosServiceDependency {
         PhotosServiceDependency(service: PhotosService.photosService)
     }
